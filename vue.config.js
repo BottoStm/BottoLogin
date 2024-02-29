4a5c498d71f874ee41a76da448765f336c4d291c
krishiv-g <steem.krishiv@gmail.com>
2024-02-29 09:23:40 +0000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   outputDir: path.resolve(__dirname, './www'),
   devServer: {
-    allowedHosts: 'all', 
-    disableHostCheck: true,
+    allowedHosts: ['all'], // Correct format: 'all' within an array
+    disableHostCheck: true, // This disables host checking, not recommended for production
   },
 };
